7c355f67570462726926ba562212be4d64eeb785
Oliver Inglis <ollie@theinglistwins.com>
2020-02-14 10:46:15 +0000

    env file created

--- src/api/youtube_api.jsx
+++ src/api/youtube_api.jsx
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-const KEY = "AIzaSyCqyDrkwkR49lrnKycgYuvOV7iKpFIrQ-A";
-
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
